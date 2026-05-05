e7d4354da4dfd685c45a1bd0d144b44187032fba
elpupin <lucas@procomdigital.net>
2026-05-05 13:40:22 -0300

    Mejora App

--- tp1prog2/src/App.java
+++ tp1prog2/src/App.java
@@ -6,7 +6,6 @@ public class App {
 
     public static void main(String[] args) {
 
-        // Seccion 2
         Usuario usuario1 = new Usuario();
         Usuario usuario2 = new Usuario("Lucas");
         Usuario usuario3 = new Usuario("Lucas", 20);
@@ -18,7 +17,6 @@ public class App {
         reporte.mostrarSecciones("Inicio", "Desarrollo", "Final");
         reporte.mostrarSecciones();
 
-        // Seccion 3
         Documento doc1 = new Factura();
         Documento doc2 = new Recibo();
 
@@ -28,7 +26,6 @@ public class App {
         Factura factura = new Factura();
         factura.procesar(true);
 
-        // Seccion 4
         factura.exportar();
         factura.registrarAuditoria();
 
